02ae2e73615a69e998d8a2e6b37a074accb4a0d7
Harun Semic <hsemic5@gmail.com>
2024-12-20 17:36:05 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+/*const distanceSlider = document.getElementById("distanceSlider");
+const distanceValue = document.getElementById("distanceValue");
+
+distanceSlider.addEventListener("input", () => {
+  distanceValue.textContent = distanceSlider.value;
+});*/
+
+
+document.getElementById("lightOnBtn").addEventListener("click", () => {
+  console.log("Light On button clicked");
+});
+
+document.getElementById("lightOffBtn").addEventListener("click", () => {
+  console.log("Light Off button clicked");
+
+});
+
+document.getElementById("openDoorBtn").addEventListener("click", () => {
+  console.log("Open Door button clicked");
+});
+
+document.getElementById("closeDoorBtn").addEventListener("click", () => {
+  console.log("Close Door button clicked");
+});
+
+document.getElementById("autoCloseBtn").addEventListener("click", () => {
+  console.log("Enable Auto-Close button clicked");
+});
+
+  
